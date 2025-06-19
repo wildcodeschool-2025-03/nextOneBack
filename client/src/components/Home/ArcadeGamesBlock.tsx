db1bf1434b09e41d0772c886b9881d7e19480d06
Dylan-Gerard <d.marcgerard@gmail.com>
2025-06-19 18:38:11 +0200

    feat: create homepage + container for next feature

---
+++ client/src/components/Home/ArcadeGamesBlock.tsx
@@ -0,0 +1,16 @@
+import "./ArcadeGamesBlock.css";
+
+export default function ArcadeGamesBlock() {
+  return (
+    <div className="arcade-games-container">
+      <div className="block-title">
+        <span className="line" />
+        <h2>LES ARCADES</h2>
+        <span className="line" />
+      </div>
+      <div className="arcade-games-content">
+        {/* carrousel prochainement ;-p */}
+      </div>
+    </div>
+  );
+}
