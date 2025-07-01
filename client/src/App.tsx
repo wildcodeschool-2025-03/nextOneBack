df873e9409fb707f35f3ae0390d249c050d22f0d
marina saraiva <marinasaraiva@hotmail.fr>
2025-07-01 15:34:59 +0200

    feat: add JWT token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import type { Auth } from "./types/auth";
 
 function App() {
+  const [auth, setAuth] = useState(null as Auth | null);
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={{ auth, setAuth }} />
     </>
   );
 }
