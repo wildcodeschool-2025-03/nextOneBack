27ea5da34148a81632879e20886ec8cd495ca7a5
marina saraiva <marinasaraiva@hotmail.fr>
2025-07-02 09:39:26 +0200

    feat: css

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import cloud from "../assets/images/cloud.png";
+import personnage from "../assets/images/monstre.png";
 import ArcadeGamesBlock from "../components/Home/ArcadeGamesBlock";
 import EventsBlock from "../components/Home/EventsBlock";
 import MiniGamesBlock from "../components/Home/MiniGamesBlock";
@@ -9,6 +11,12 @@ export default function HomePage() {
     <main className="homepage">
       <div className="hero-banner">
         <h1>Next One</h1>
+        <img src={cloud} alt="nuage flottant" className="cloud-home1" />
+        <img
+          src={personnage}
+          alt="petit personnage retro"
+          className="perso-retro"
+        />
         <p>
           Lance ta partie.
           <br />
