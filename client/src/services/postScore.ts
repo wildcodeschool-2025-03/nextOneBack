c6c314dcb3a1194b836903d46ebd5af6cc50e294
MBoussel <mb.boussel@gmail.com>
2025-07-15 15:29:29 +0200

    fix: add score with coockies

--- client/src/services/postScore.ts
+++ client/src/services/postScore.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { toast } from "react-toastify";
 
 type DataParty = {
   id_game: number;
@@ -9,17 +8,11 @@ type DataParty = {
 
 export async function postScore(dataParty: DataParty) {
   try {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      toast.error("utilisateur non connecté ❌");
-    }
     const response = await axios.post(
       `${import.meta.env.VITE_API_URL}/api/partys`,
       dataParty,
       {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+        withCredentials: true,
       },
     );
     return response.data;
