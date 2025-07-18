3ec2ecdd452fb5583bad40e35ab165d73ce8a56f
Dylan-Gerard <d.marcgerard@gmail.com>
2025-07-18 10:41:18 +0200

    feat: fix add inner join

--- server/src/modules/game/gameRepository.ts
+++ server/src/modules/game/gameRepository.ts
@@ -9,7 +9,7 @@ const findTopRankingByGameId = async (gameId: number) => {
   const [rows] = await db.query(
     `SELECT u.pseudo AS username, p.score
      FROM Party p
-     JOIN User u ON p.id_user = u.id
+     INNER JOIN User u ON p.id_user = u.id
      WHERE p.id_game = ?
      ORDER BY p.score DESC
      LIMIT 3`,
